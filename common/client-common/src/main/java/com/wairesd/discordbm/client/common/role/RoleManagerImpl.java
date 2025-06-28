4b4547762d3038adbcfecb8b4d9f4030a6a2bdad
1wairesd <1wairesd.com.industries@gmail.com>
2025-06-28 12:05:39 +0300

    replace

--- common/client-common/src/main/java/com/wairesd/discordbm/client/common/role/RoleManagerImpl.java
+++ common/client-common/src/main/java/com/wairesd/discordbm/client/common/role/RoleManagerImpl.java
@@ -47,7 +47,6 @@ public class RoleManagerImpl implements RoleManager {
         }
     }
 
-    // Этот метод должен вызываться из MessageHandler при получении ответа
     public void handleRoleActionResponse(RoleActionResponse response) {
         CompletableFuture<Boolean> future = pendingRequests.remove(response.getRequestId());
         if (future != null) {
