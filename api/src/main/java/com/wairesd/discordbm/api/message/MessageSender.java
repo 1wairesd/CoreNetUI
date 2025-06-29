760e89b16f183625e099a1632c3a029804917c8f
1wairesd <1wairesd.com.industries@gmail.com>
2025-06-29 23:54:49 +0300

    replace

--- api/src/main/java/com/wairesd/discordbm/api/message/MessageSender.java
+++ api/src/main/java/com/wairesd/discordbm/api/message/MessageSender.java
@@ -10,92 +10,54 @@ import java.util.List;
  * Interface for sending messages to Discord
  */
 public interface MessageSender {
-    
+
+    // --- Response ---
+
     /**
      * Send a text message in response to a command
-     * 
+     *
      * @param requestId The request ID of the command
      * @param message The message to send
      */
     void sendResponse(String requestId, String message);
-    
+
     /**
      * Send an embed in response to a command
-     * 
+     *
      * @param requestId The request ID of the command
      * @param embed The embed to send
      */
     void sendResponse(String requestId, Embed embed);
-    
+
     /**
      * Send an embed with buttons in response to a command
-     * 
+     *
      * @param requestId The request ID of the command
      * @param embed The embed to send
      * @param buttons The buttons to add to the message
      */
     void sendResponseWithButtons(String requestId, Embed embed, List<Button> buttons);
-    
+
     /**
      * Send a form in response to a command
-     * 
+     *
      * @param requestId The request ID of the command
      * @param form The form to send
      */
     void sendForm(String requestId, Form form);
-    
+
     /**
      * Send a form with a message in response to a command
-     * 
+     *
      * @param requestId The request ID of the command
      * @param message The message to send with the form
      * @param form The form to send
      */
     void sendFormWithMessage(String requestId, String message, Form form);
-    
-    /**
-     * Send a direct message to a user
-     * 
-     * @param userId The ID of the user to send the message to
-     * @param message The message to send
-     */
-    void sendDirectMessage(String userId, String message);
-    
-    /**
-     * Send an embed as a direct message to a user
-     * 
-     * @param userId The ID of the user to send the message to
-     * @param embed The embed to send
-     */
-    void sendDirectMessage(String userId, Embed embed);
-    
-    /**
-     * Send a message to a channel
-     * 
-     * @param channelId The ID of the channel to send the message to
-     * @param message The message to send
-     */
-    void sendChannelMessage(String channelId, String message);
-    
-    /**
-     * Send an embed to a channel
-     * 
-     * @param channelId The ID of the channel to send the message to
-     * @param embed The embed to send
-     */
-    void sendChannelMessage(String channelId, Embed embed);
-    
-    /**
-     * Send an embed with buttons to a channel
-     * 
-     * @param channelId The ID of the channel to send the message to
-     * @param embed The embed to send
-     * @param buttons The buttons to add to the message
-     */
-    void sendChannelMessageWithButtons(String channelId, Embed embed, List<Button> buttons);
 
     /**
      * Send a text message in response to a command with visibility control
+     *
      * @param requestId The request ID of the command
      * @param message The message to send
      * @param ephemeral Whether the message should be ephemeral (private)
@@ -104,6 +66,7 @@ public interface MessageSender {
 
     /**
      * Send an embed in response to a command with visibility control
+     *
      * @param requestId The request ID of the command
      * @param embed The embed to send
      * @param ephemeral Whether the message should be ephemeral (private)
@@ -112,6 +75,7 @@ public interface MessageSender {
 
     /**
      * Send an embed with buttons in response to a command with visibility control
+     *
      * @param requestId The request ID of the command
      * @param embed The embed to send
      * @param buttons The buttons to add to the message
@@ -121,6 +85,7 @@ public interface MessageSender {
 
     /**
      * Send a form in response to a command with visibility control
+     *
      * @param requestId The request ID of the command
      * @param form The form to send
      * @param ephemeral Whether the form modal should be ephemeral (private)
@@ -129,10 +94,56 @@ public interface MessageSender {
 
     /**
      * Send a form with a message in response to a command with visibility control
+     *
      * @param requestId The request ID of the command
      * @param message The message to send with the form
      * @param form The form to send
      * @param ephemeral Whether the form modal should be ephemeral (private)
      */
     void sendFormWithMessage(String requestId, String message, Form form, boolean ephemeral);
-} 
+
+    // --- Direct Message ---
+
+    /**
+     * Send a direct message to a user
+     *
+     * @param userId The ID of the user to send the message to
+     * @param message The message to send
+     */
+    void sendDirectMessage(String userId, String message);
+
+    /**
+     * Send an embed as a direct message to a user
+     *
+     * @param userId The ID of the user to send the message to
+     * @param embed The embed to send
+     */
+    void sendDirectMessage(String userId, Embed embed);
+
+    // --- Channel Message ---
+
+    /**
+     * Send a message to a channel
+     *
+     * @param channelId The ID of the channel to send the message to
+     * @param message The message to send
+     */
+    void sendChannelMessage(String channelId, String message);
+
+    /**
+     * Send an embed to a channel
+     *
+     * @param channelId The ID of the channel to send the message to
+     * @param embed The embed to send
+     */
+    void sendChannelMessage(String channelId, Embed embed);
+
+    /**
+     * Send an embed with buttons to a channel
+     *
+     * @param channelId The ID of the channel to send the message to
+     * @param embed The embed to send
+     * @param buttons The buttons to add to the message
+     */
+    void sendChannelMessageWithButtons(String channelId, Embed embed, List<Button> buttons);
+}
