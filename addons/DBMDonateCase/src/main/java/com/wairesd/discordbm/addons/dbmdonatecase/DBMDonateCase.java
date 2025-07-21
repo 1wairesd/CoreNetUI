1194f0f6736ed52d101c8ca7f7908e9bed8a5f7a
1wairesd <149899427+1wairesd@users.noreply.github.com>
2025-07-21 14:10:45 +0000

    replace

--- addons/DBMDonateCase/src/main/java/com/wairesd/discordbm/addons/dbmdonatecase/DBMDonateCase.java
+++ addons/DBMDonateCase/src/main/java/com/wairesd/discordbm/addons/dbmdonatecase/DBMDonateCase.java
@@ -34,77 +34,77 @@ public final class DBMDonateCase extends JavaPlugin {
 
         WebhookTriggersConfig triggersConfig = new WebhookTriggersConfig(this);
         caseOpenListener = new CaseOpenListener(
-            dbmApi,
-            getLogger(),
-            triggersConfig,
-            openManager,
-            api.getCaseManager()
+                dbmApi,
+                getLogger(),
+                triggersConfig,
+                openManager,
+                api.getCaseManager()
         );
         api.getEventBus().register(caseOpenListener);
         dbmApi.getEventBus().register(new DBMReloadListener(messages));
 
         CommandOption playerOpt = dbmApi.getCommandRegistration().createOptionBuilder()
-            .name("player")
-            .description("Ник игрока в майнкрафте")
-            .type("STRING")
-            .required(true)
-            .build();
+                .name("player")
+                .description("Ник игрока в майнкрафте")
+                .type("STRING")
+                .required(true)
+                .build();
 
         Command dcStatsCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dcstats")
-            .description("Показать количество ключей DonateCase у игрока")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .options(List.of(playerOpt))
-            .build();
+                .name("dcstats")
+                .description("Показать количество ключей DonateCase у игрока")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .options(List.of(playerOpt))
+                .build();
 
         dbmApi.getCommandRegistration().registerCommand(dcStatsCmd, new DcStatsCommandHandler(api, dbmApi, keyManager, openManager, messages));
 
         Command dcCasesCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dccases")
-            .description("Список всех кейсов DonateCase")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .build();
+                .name("dccases")
+                .description("Список всех кейсов DonateCase")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcCasesCmd, new DcCasesCommandHandler(api, dbmApi, messages));
 
         Command dcTopCasesCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dctopcases")
-            .description("Топ 10 кейсов по количеству открытий")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .build();
+                .name("dctopcases")
+                .description("Топ 10 кейсов по количеству открытий")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcTopCasesCmd, new DcTopCasesCommandHandler(api, dbmApi, messages));
 
         CommandOption caseOpt = dbmApi.getCommandRegistration().createOptionBuilder()
-            .name("case")
-            .description("Тип кейса (casetype)")
-            .type("STRING")
-            .required(true)
-            .build();
+                .name("case")
+                .description("Тип кейса (casetype)")
+                .type("STRING")
+                .required(true)
+                .build();
         Command dcTopPlayersCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dctopplayers")
-            .description("Топ 10 игроков по открытию выбранного кейса")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .options(List.of(caseOpt))
-            .build();
+                .name("dctopplayers")
+                .description("Топ 10 игроков по открытию выбранного кейса")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .options(List.of(caseOpt))
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcTopPlayersCmd, new DcTopPlayersCommandHandler(api, dbmApi, messages));
 
         Command dcTopPlayersAllCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dctopplayersall")
-            .description("Топ 10 игроков по всем открытым кейсам")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .build();
+                .name("dctopplayersall")
+                .description("Топ 10 игроков по всем открытым кейсам")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcTopPlayersAllCmd, new DcTopPlayersAllCommandHandler(api, dbmApi, messages));
 
         Command dcLastDropsCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dclastdrops")
-            .description("Показать последние 10 открытий кейсов на сервере (кто, что, когда)")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .build();
+                .name("dclastdrops")
+                .description("Показать последние 10 открытий кейсов на сервере (кто, что, когда)")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcLastDropsCmd, new DcLastDropsCommandHandler(api, dbmApi, messages));
 
         CommandOption playerOptHistory = dbmApi.getCommandRegistration().createOptionBuilder()
@@ -123,16 +123,16 @@ public final class DBMDonateCase extends JavaPlugin {
         dbmApi.getCommandRegistration().registerCommand(dcHistoryCmd, new DcHistoryCommandHandler(api, dbmApi, messages));
 
         Command dcHelpCmd = dbmApi.getCommandRegistration().createCommandBuilder()
-            .name("dchelp")
-            .description("Показать все команды DBMDonateCase")
-            .pluginName("DBMDonateCase")
-            .context("both")
-            .build();
+                .name("dchelp")
+                .description("Показать все команды DBMDonateCase")
+                .pluginName("DBMDonateCase")
+                .context("both")
+                .build();
         dbmApi.getCommandRegistration().registerCommand(dcHelpCmd, (cmd, options, requestId) -> {
             Embed embed = dbmApi.createEmbedBuilder()
-                .setTitle(messages.get("dchelp_title"))
-                .setDescription(messages.get("dchelp_list"))
-                .build();
+                    .setTitle(messages.get("dchelp_title"))
+                    .setDescription(messages.get("dchelp_list"))
+                    .build();
             dbmApi.getMessageSender().sendResponse(requestId, embed);
         });
 
