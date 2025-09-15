ea75e3ec528b30a99f8e3e28631b3b357b73f468
1wairesd <1wairesd.com.industries@gmail.com>
2025-09-15 23:50:27 +0300

    Add Bukkit support with initial bootstrap class and configuration files

---
+++ wrappers/bukkit/src/main/java/com/wairesdindustries/flexcore/bukkit/FCBukkitBootstrap.java
@@ -0,0 +1,15 @@
+package com.wairesdindustries.flexcore.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FCBukkitBootstrap extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
--- wrappers/velocity/src/main/java/com/wairesdindustries/flexcore/velocity/FCVelocityBootstrap.java
+++ wrappers/velocity/src/main/java/com/wairesdindustries/flexcore/velocity/FCVelocityBootstrap.java
@@ -3,6 +3,7 @@ package com.wairesdindustries.flexcore.velocity;
 import com.google.inject.Inject;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyShutdownEvent;
 import com.velocitypowered.api.plugin.Plugin;
 import org.slf4j.Logger;
 
@@ -17,5 +18,9 @@ public class FCVelocityBootstrap {
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
+    }
+
+    @Subscribe
+    public void onProxyShutdown(ProxyShutdownEvent event) {
     }
 }
